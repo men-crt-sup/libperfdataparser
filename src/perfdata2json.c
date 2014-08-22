9d1a8f0b5b21a96c5a2ea2f8ca48c0d96542ae54
urba <s.urbanovski@ac-nancy-metz.fr>
2014-08-22 16:19:27 +0200

    some renaming and fixes

--- src/perfdata2json.c
+++ src/perfdata2json.c
@@ -44,26 +44,22 @@ int main(int argc, char **argv) {
 	
 		size_t len = strlen(buffer);
 	
-		typePerfdata *perfdata1 = NULL;
+		typePerfdata *perfStuct = NULL;
 	
-		//FIXME: Iterate ?
 		if ( len > 0 && buffer[len-1] == '\n') {
 			buffer[len-1] = '\0';
 			len--;
-		} else {
-			fprintf(stderr, "Warn: No new line\n");
 		}
 	
 		while ( current < len ) {
 			TRACE ("  current = %d",(int)current);
 			
-			parsedCount = parsePerfdata(buffer + current, &errmsg, &perfdata1);
+			parsedCount = parsePerfdata(buffer + current, &errmsg, &perfStuct);
 			parsedString = strndup(buffer + current,parsedCount);
 			current += parsedCount;
 			
 			if ( errmsg != NULL ) {
 				
-				parsedString = strdup(buffer + current);
 				fprintf(stderr,"ERROR: parse error : %s\n", errmsg);
 				fprintf(stderr,"  Parsed string : %s\n", parsedString);
 				free(errmsg);
@@ -71,40 +67,41 @@ int main(int argc, char **argv) {
 				continue;
 			}
 			free(parsedString);
-			if ( perfdata1 != NULL ) {
+			if ( perfStuct != NULL ) {
 				
 				// Do something usefull
 				
-				TRACE ("Got something usable in '%s' : '%s' = %f %s", perfdata1->data, perfdata1->name, perfdata1->value, perfdata1->unit);
-				TRACE ("  min = %f, max = %f", perfdata1->min, perfdata1->max );
-				TRACE ("  warn : min = %f , max = %f (%d)", perfdata1->warn_min, perfdata1->warn_max, perfdata1->warn_ins );
-				TRACE ("  crit : min = %f , max = %f (%d)", perfdata1->crit_min, perfdata1->crit_max, perfdata1->crit_ins );
+				TRACE ("Got something usable in '%s' : '%s' = %f %s", perfStuct->data, perfStuct->name, perfStuct->value, perfStuct->unit);
+				TRACE ("  min = %f, max = %f", perfStuct->min, perfStuct->max );
+				TRACE ("  warn : min = %f , max = %f (%d)", perfStuct->warn_min, perfStuct->warn_max, perfStuct->warn_ins );
+				TRACE ("  crit : min = %f , max = %f (%d)", perfStuct->crit_min, perfStuct->crit_max, perfStuct->crit_ins );
 				
 				json = json_object_new_object();
-				json_object_object_add(json, "name", json_object_new_string(perfdata1->name));
-				json_object_object_add(json, "value", json_object_new_double(perfdata1->value));
+				json_object_object_add(json, "name", json_object_new_string(perfStuct->name));
+				json_object_object_add(json, "value", json_object_new_double(perfStuct->value));
 				
-				if ( perfdata1->unit != NULL) {
-					json_object_object_add(json, "unit", json_object_new_string(perfdata1->unit));
+				if ( perfStuct->unit != NULL) {
+					json_object_object_add(json, "unit", json_object_new_string(perfStuct->unit));
 				}
 				
-				json_object_object_add(json, "warn_min", json_object_new_double(perfdata1->warn_min));
-				json_object_object_add(json, "warn_max", json_object_new_double(perfdata1->warn_max));
-				json_object_object_add(json, "warn_range", json_object_new_int(perfdata1->warn_range));
+				json_object_object_add(json, "w_min", json_object_new_double(perfStuct->warn_min));
+				json_object_object_add(json, "w_max", json_object_new_double(perfStuct->warn_max));
+				json_object_object_add(json, "w_range", json_object_new_int(perfStuct->warn_range));
 				
-				json_object_object_add(json, "crit_min", json_object_new_double(perfdata1->crit_min));
-				json_object_object_add(json, "crit_max", json_object_new_double(perfdata1->crit_max));
-				json_object_object_add(json, "crit_range", json_object_new_int(perfdata1->crit_range));
+				json_object_object_add(json, "c_min", json_object_new_double(perfStuct->crit_min));
+				json_object_object_add(json, "c_max", json_object_new_double(perfStuct->crit_max));
+				json_object_object_add(json, "c_range", json_object_new_int(perfStuct->crit_range));
 				
-				if ( ! isnan(perfdata1->min) ) {
-					json_object_object_add(json, "min", json_object_new_double(perfdata1->min));
+				if ( ! isnan(perfStuct->min) ) {
+					json_object_object_add(json, "min", json_object_new_double(perfStuct->min));
 				}
-				if ( ! isnan(perfdata1->max) ) {
-					json_object_object_add(json, "max", json_object_new_double(perfdata1->max));
+				if ( ! isnan(perfStuct->max) ) {
+					json_object_object_add(json, "max", json_object_new_double(perfStuct->max));
 				}
 				
-				if ( perfdata1->data != NULL) {
-					json_object_object_add(json, "data", json_object_new_string(perfdata1->data));
+				// TODO: Make this one optional :
+				if ( perfStuct->data != NULL) {
+					json_object_object_add(json, "data", json_object_new_string(perfStuct->data));
 				}
 				
 				printf("%s\n", json_object_get_string(json));
@@ -117,12 +114,12 @@ int main(int argc, char **argv) {
 					free(errmsg);
 				}
 			
-				free(perfdata1->name);
-				if ( perfdata1->unit != NULL) {
-					free(perfdata1->unit);
+				free(perfStuct->name);
+				if ( perfStuct->unit != NULL) {
+					free(perfStuct->unit);
 				}
-				if ( perfdata1->data != NULL) {
-					free(perfdata1->data);
+				if ( perfStuct->data != NULL) {
+					free(perfStuct->data);
 				}
 			}
 //             sleep(1);
